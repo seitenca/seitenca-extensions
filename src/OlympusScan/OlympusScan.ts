633fa0a0920f8aed53b5447c2ca114a55588b64b
seitenca <alexdltg@protonmail.com>
2023-08-14 17:16:28 +0200

    fix error and warn that novels do not work

--- src/OlympusScan/OlympusScan.ts
+++ src/OlympusScan/OlympusScan.ts
@@ -33,7 +33,7 @@ const OS_DOMAIN = 'https://olympusv2.gg'
 const OS_API_DOMAIN = 'https://dashboard.olympusv2.gg/api'
 
 export const OlympusScanInfo: SourceInfo = {
-    version: '1.0.10',
+    version: '1.0.11',
     name: 'OlympusScan',
     icon: 'icon.png',
     author: 'Seitenca',
@@ -84,22 +84,59 @@ export class OlympusScan implements SearchResultsProviding, MangaProviding, Chap
         try {
             return JSON.parse(response.data as string)
         } catch (e) {
-            throw new Error(`${e}`)
+            throw new Error('URL not found')
         }
     }
 
     async getMangaDetails(mangaId: string): Promise<SourceManga> {
-        const data = await this.getData(`${OS_API_DOMAIN}/series/${mangaId}`)
+        let data
+        try {
+            const response = await this.requestManager.schedule(App.createRequest({
+                url: `${OS_API_DOMAIN}/series/${mangaId}?page=1&direction=desc&type=comic`,
+                method: 'GET'
+            }), 1)
+            data = JSON.parse(response.data as string)
+        } catch (e) {
+            const response = await this.requestManager.schedule(App.createRequest({
+                url: `${OS_API_DOMAIN}/series/${mangaId}?page=1&direction=desc&type=novel`,
+                method: 'GET'
+            }), 1)
+            data = JSON.parse(response.data as string)
+        }
         return parseMangaDetails(data.data, mangaId)
     }
 
     async getChapters(mangaId: string): Promise<Chapter[]> {
         const chapters: Chapter[] = []
-        const total_pages_data = await this.getData(`${OS_API_DOMAIN}/series/${mangaId}/chapters`)
+        let total_pages_data
+        let type
+        try {
+            const response = await this.requestManager.schedule(App.createRequest({
+                url: `${OS_API_DOMAIN}/series/${mangaId}/chapters?page=1&direction=desc&type=comic`,
+                method: 'GET'
+            }), 1)
+
+            total_pages_data = JSON.parse(response.data as string)
+            type = 'comic'
+        } catch (e) {
+            const response = await this.requestManager.schedule(App.createRequest({
+                url: `${OS_API_DOMAIN}/series/${mangaId}/chapters?page=1&direction=desc&type=novel`,
+                method: 'GET'
+            }), 1)
+
+            total_pages_data = JSON.parse(response.data as string)
+            type = 'novel'
+        }
+
         const total_pages: number = total_pages_data.meta.last_page ?? 1
 
         for (let index = 1; index <= total_pages; index++) {
-            const data = await this.getData(`${OS_API_DOMAIN}/series/${mangaId}/chapters?page=${index}`)
+            let data
+            if (type == 'comic') {
+                data = await this.getData(`${OS_API_DOMAIN}/series/${mangaId}/chapters?page=${index}&direction=desc&type=comic`)
+            } else {
+                data = await this.getData(`${OS_API_DOMAIN}/series/${mangaId}/chapters?page=${index}&direction=desc&type=novel`)
+            }
             for (const chapter of data.data) {
                 const number = Number(chapter.name)
                 const title = `Chapter ${number}`
@@ -122,11 +159,27 @@ export class OlympusScan implements SearchResultsProviding, MangaProviding, Chap
     }
 
     async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
-        const data = await this.getData(`${OS_API_DOMAIN}/series/${mangaId}/chapters/${chapterId}`)
+        let data
+        let pages
+        try {
+            const response = await this.requestManager.schedule(App.createRequest({
+                url: `${OS_API_DOMAIN}/series/${mangaId}/chapters/${chapterId}`,
+                method: 'GET'
+            }), 1)
+            data = JSON.parse(response.data as string)
+            pages = data.chapter.pages
+        } catch (e) {
+            const response = await this.requestManager.schedule(App.createRequest({
+                url: `${OS_API_DOMAIN}/series/${mangaId}/chapters/${chapterId}?type=novel`,
+                method: 'GET'
+            }), 1)
+            data = JSON.parse(response.data as string)
+            pages = ['https://media.discordapp.net/attachments/981559883033870358/1140673174904778872/gomen.webp']
+        }
         return App.createChapterDetails({
             id: chapterId,
             mangaId: mangaId,
-            pages: data.chapter.pages
+            pages: pages
         })
     }
 
@@ -221,7 +274,7 @@ export class OlympusScan implements SearchResultsProviding, MangaProviding, Chap
         }
         const typeStatus: Tag[] = []
         typeStatus.push({ id: 'comic', label: 'Comic' })
-        typeStatus.push({ id: 'novel', label: 'Novela' })
+        typeStatus.push({ id: 'novel', label: 'Novela (broken)' })
 
         return [
             App.createTagSection({ id: '0', label: 'genres', tags: arrayGenres.map(x => App.createTag(x)) }),
